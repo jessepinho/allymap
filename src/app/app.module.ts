5d0b5f754023340e1baf41369edd8cf9b0d72363
Jesse Pinho <jesse@jessepinho.com>
2016-11-13 20:33:04 +0100

    feat: Include AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import { AngularFireModule } from 'angularfire2';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AgmCoreModule } from 'angular2-google-maps/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCo4cwQvRqsIKzyEqkkXRSPpIu8KM-XCKk",
+  authDomain: "ally-map.firebaseapp.com",
+  databaseURL: "https://ally-map.firebaseio.com",
+  storageBucket: "ally-map.appspot.com",
+  messagingSenderId: "37098180673"
+};
 
 @NgModule({
   declarations: [
@@ -16,6 +25,7 @@ import { MapComponent } from './map/map.component';
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCaHXS-FjKUymJzEjpl468Fkjd5EX3RYi4',
     }),
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
     FormsModule,
     HttpModule
