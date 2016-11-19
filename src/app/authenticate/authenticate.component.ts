bf6877b90e9c15e281ad2a7e5c82afd6e862ec0c
Jesse Pinho <jesse@jessepinho.com>
2016-11-19 01:59:10 +0100

    Improve fetching of user data; show photo

--- src/app/authenticate/authenticate.component.ts
+++ src/app/authenticate/authenticate.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
 import { AngularFire } from 'angularfire2';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html',
   styleUrls: ['./authenticate.component.css'],
 })
-export class AuthenticateComponent {
+export class AuthenticateComponent implements OnInit {
+  private displayName: string;
+  private email: string;
+  private isAuthenticated: boolean = false;
+  private isAuthenticating: boolean = false;
+  private photoURL: string;
+
   constructor(
     private af: AngularFire
   ) {}
+
+  private authenticate() {
+    this.isAuthenticating = true;
+
+    this.af.auth
+      .subscribe(
+        (authState) => {
+          if (authState) {
+            const { auth: { displayName, email, photoURL } } = authState;
+            this.displayName = displayName;
+            this.email = email;
+            this.photoURL = photoURL;
+            this.isAuthenticated = true;
+          } else {
+            this.isAuthenticated = false;
+          }
+          this.isAuthenticating = false;
+        },
+        () => {
+          this.isAuthenticating = false;
+          this.isAuthenticated = false;
+        }
+      );
+  }
+
+  ngOnInit() {
+    this.authenticate();
+  }
 
   // tslint:disable-next-line:no-unused-variable
   private logIn() {
